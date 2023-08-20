bbfdd664267769eeb82a8b72be294adde26f9ecd
Aman L <amankhanter14@gmail.com>
2023-08-20 12:58:11 +0530

    Adjusted the submission schema and some work done

---
+++ api/submission.js
@@ -0,0 +1,8 @@
+import submission_db from "../schema/submission";
+
+class submission{
+    async create(req,res){
+        const {id,user,language,code,question_id} = req.body();
+        
+    }
+}
--- schema/submission.js
+++ schema/submission.js
@@ -1,10 +1,15 @@
-import {schema} from mongoose
+import {schema,model} from mongoose
 
 const submission_schema = schema({
-    language : {type:String, required:true},
-    code : {type:String, required:true},
-    question_id : {type:Number,required:true},
-    //test_cases : {type:[<document name>.test_cases]} //In future it can call the questions databases for the test cases that has to be tested and store it as an array of objects
+    id : {type:Number},
+    user : {type:Number,required:true},
+    language : {type:String,required:true},
+    code : {type:String,required:true},
+    pass : {type:Boolean, default:false},
+    timestamp : {type:Date},
+    question_id : {type:Number} //this will act as an indicator to which question in the question DB shld be tested against the code
 });
 
+const submission_db = model("Submissions",submission_schema); 
 
+export default submission_db;
