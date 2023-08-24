73598f8bb75dc73017c2cd33821bc18199507c48
prateek-srivastava001 <prateeksrivastava201@gmail.com>
2023-08-25 01:40:41 +0530

    feat: added auth routes

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const os = require("os");
-require("dotenv").config();
-
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoute = require('./api/routes/auth');
 const app = express();
+const port = 3000;
 
-// Read MongoDB connection details from environment variables
-const dbHost = process.env.DB_HOST;
-const dbPort = process.env.DB_PORT;
-const dbName = process.env.DB_NAME;
-
-// Construct the MongoDB URI
-const mongoURI = `mongodb://${dbHost}:${dbPort}/${dbName}`;
-
-// Connecting to mongoDB
-mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
-
-// Using middleware
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(authRoute);
 
-// Setting up routes
-app.get("/ping", (req, res) => {
-  res.status(200).json({ msg: "ping", hostname: os.hostname() });
+mongoose.connect('mongodb+srv://tnvjain2003:z6sr6BKmWm8PQmmx@cluster0.g8mzbed.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('connected to db');
+})
+.catch(error => {
+  console.error('error connecting', error);
 });
 
+app.get('/', (req, res) => {
+  res.send('hello');
+});
 
-// Starting Server
-app.listen(8080, () => {
-  console.log("Server started at port: 8080");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
