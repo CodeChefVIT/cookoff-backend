e5bbc0a45494cf615335e883670223e4ee99f545
Akshat Gupta <84951451+Oik17@users.noreply.github.com>
2023-08-24 23:30:14 +0530

    Add files via upload

---
+++ api/ques.js
@@ -0,0 +1,51 @@
+const mongoose=require('mongoose')
+
+
+const quesSchema = new mongoose.Schema({
+    //id: false,
+    question: { 
+        type: String,
+        required: true,
+        unique: true 
+    },
+    id: {
+         type: Number,
+        required: true
+    },
+    inputFormat: [
+      {type: String}
+    ],
+    outputFormat: [
+      {type: String}
+    ],
+    constraints: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    sampleTestInput: { 
+        type: String, 
+        required: true 
+    },
+    sampleTestOutput: {
+        type: String, 
+        required: true 
+    },
+    timeLimit: { 
+        type: String, 
+        required: true 
+    },
+    sourceLimit: { 
+        type: String, 
+        required: true 
+    },
+    round: {
+      type: Number,
+      required: true 
+    },
+    testCases: [{ type: mongoose.Schema.Types.ObjectId, ref: "TestCase" }],
+  });
+
+
+module.exports= mongoose.model("Question",quesSchema)   
