2d307ae87f1ddbec44c6227bf462e84aa80ea648
Akshat Gupta <akshat.g1707@gmail.com>
2023-08-26 00:06:24 +0530

    Created create and read for questions model

--- api/controllers/questions.js
+++ api/controllers/questions.js
@@ -1,142 +1,79 @@
-const mongoose=require('mongoose')
-const ques= require("../models/ques")
-const express=require("express");
-const bodyParser=require("body-parser");
-//const TestCases=require("./TestCase");
-//const getTestCases=require("./testCases");
-const app=express();
-const port=5000; 
-mongoose.connect("mongodb://127.0.0.1/QuestionsTesting");
+const ques = require("./ques");
 
-//console.log(getTestCases( ));
-app.use(bodyParser.urlencoded({extended: true}));
-//getQuestionByID(1);
-app.post("/get",(req,res)=>{
-    console.log("post");
-    getAll(req,res);
-})
-
-app.post("/add",(req,res)=>{
-    createQuestion(req,res);
-})
-
-app.post("/getOne",(req,res)=>{
-    getQuestionByID(req,res);
-})
-
-app.post("/getRound",(req,res)=>{
-  getByRound(req,res);
-  
-})
-// async function createTestCases(req, res) {
-//   try {
-//      const testCase = await TestCaseModel.create({
-//       expectedOutput: req.body.expectedOutput,
-//       input: req.body.input,
-//       number: req.body.number,
-//       hidden: req.body.hidden,
-//       time: req.body.time,
-//       memory: req.body.memory,
-//       question: req.body.question
-//     });
-//     await testCase.save();
-
-//     return res.status(201).json(testCase);
-//   } catch (error) {
-//     return res.status(500).json({
-//       message: error.message,
-//     })
-//   }
-// }
-async function getQuestionByID(req,res){
-    try{
-        const questions=await ques.where("id").equals(req.body.id);
-        console.log(questions);
-        return res.status(201).json(questions);
-    }
-    catch (error) {
-        return res.status(500).json({
-          message: "Something went wrong",
-        });
-      }
-}
-
-async function getAll(req,res){
-    try{
-        const questionsAll=await ques.find();
-        console.log(questionsAll);
-        //res.status(200).send("Status Working");
-        return res.status(201).json(questionsAll);
-    }
-    catch (error) {
-        return res.status(500).json({
-          message: "Something went wrong",
-        });
-      }
-}
-
-async function getByRound(req,res){
-  try{
-    const questionByRound= await ques.where("round").equals(req.body.round);
-    console.log(questionByRound);
-    return res.status(201).json(questionByRound);
-  }
-  catch(error){
+async function getQuestionByID(req, res) {
+  try {
+    const questions = await ques
+      .where("id")
+      .equals(req.body.id)
+      .populate("testCases");
+    console.log(questions);
+    return res.status(201).json(questions);
+  } catch (error) {
     return res.status(500).json({
       message: error.message,
-    })
+    });
   }
 }
 
+async function getAll(req, res) {
+  try {
+    const questionsAll = await ques.find().populate("testCases");
 
-async function createQuestion(req, res) {   //admin only
-    try {
-    //console.log(req.body);
-      //var Ques;
-      // const testCase= await testCases.create({
-      //   expectedOutput: req.body.expectedOutput,
-      //   input: req.body.input,
-      //   number: req.body.number,
-      //   hidden: req.body.hidden,
-      //   time: req.body.time,
-      //   memory: req.body.memory,
-      //   explanation: req.body.explanation,
-      //   question: req.body.question,
+    console.log(questionsAll);
 
-      // });
-      // var arr=[];
-      // for(var i=0;i<testCases.length;i++){
-      //   if (testCases[i].question==req.body.question){
-      //     arr.push(testCases[i].id);
-      //   }
-      // }
-      // console.log(arr);
-      const Ques = await ques.create({
-        question: req.body.question,
-        id: req.body.id,
-        inputFormat: req.body.inputFormat,
-        outputformat: req.body.outputFormat,
-        constraints: req.body.constraints,
-        sampleTestInput: req.body.sampleTestInput,
-        sampleTestOutput: req.body.sampleTestOutput,
-        timeLimit: req.body.timeLimit,
-        sourceLimit: req.body.sourceLimit,
-        round: req.body.round,
-        //testCases: arr,
-        //testCases: testCase.id,
-      });
-      await Ques.save();
-      //await testCase.save();
-      //console.log(testCase);
-      console.log(Ques);
-      return res.status(201).json(Ques);
-    } catch (error) {
-      return res.status(500).json({
-        message: error.message,
-      });
-    }
+    return res.status(201).json(questionsAll);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something went wrong",
+    });
   }
+}
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
+async function getByRound(req, res) {
+  try {
+    const questionByRound = await ques
+      .where("round")
+      .equals(req.body.round)
+      .populate("testCases");
+    console.log(questionByRound);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+}
+
+async function createQuestion(req, res) {
+  //admin only
+  try {
+    const Ques = await ques.create({
+      name: req.body.name,
+      id: req.body.id,
+      inputFormat: req.body.inputFormat,
+      outputformat: req.body.outputFormat,
+      constraints: req.body.constraints,
+      sampleTestInput: req.body.sampleTestInput,
+      sampleTestOutput: req.body.sampleTestOutput,
+      timeLimit: req.body.timeLimit,
+      sourceLimit: req.body.sourceLimit,
+      round: req.body.round,
+      objective: req.body.objective,
+      testCases: [],
+    });
+    await Ques.save();
+
+    console.log(Ques);
+    return res.status(201).json(Ques);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+}
+
+module.exports = {
+  createQuestion,
+  getAll,
+  getByRound,
+  getQuestionByID,
+};
--- api/models/ques.js
+++ api/models/ques.js
@@ -1,31 +1,19 @@
-const mongoose = require('mongoose')
-//const testCases=require("/TestCase");
+const mongoose = require("mongoose");
 
-const TestCaseSchema = new mongoose.Schema({
-  expectedOutput: { type: String, required: true },
-  input: { type: String, required: true },
-  number: { type: Number, required: true },
-  hidden: { type: Boolean, default: false },
-  time: { type: Number, default: 0 },
-  memory: { type: Number, default: 0 },
-  explanation: { type: String },
-  //question: { type: mongoose.Schema.Types.ObjectId, ref: "Question" }
-  question: {type: String}
-});
-//module.exports = mongoose.model("Question", quesSchema)   
+
 const quesSchema = new mongoose.Schema({
   //id: false,
-  question: {type: String, required: true, unique: true },
+  question: { type: String, required: true, unique: true },
   //id: {type: Number, required: true },
   inputFormat: [{ type: String }],
   outputFormat: [{ type: String }],
-  constraints: [{type: String, required: true,},],
-  sampleTestInput: {type: String, required: true},
-  sampleTestOutput: {type: String, required: true},
-  timeLimit: {type: String, required: true},
-  sourceLimit: {type: String, required: true},
-  round: {type: Number, required: true},
+  constraints: [{ type: String, required: true }],
+  sampleTestInput: { type: String, required: true },
+  sampleTestOutput: { type: String, required: true },
+  timeLimit: { type: String, required: true },
+  sourceLimit: { type: String, required: true },
+  round: { type: Number, required: true },
   testCases: [{ type: mongoose.Schema.Types.ObjectId, ref: "TestCaseSchema" }],
 });
 
-module.exports = mongoose.model("Question", quesSchema)   
+module.exports = mongoose.model("Question", quesSchema);
