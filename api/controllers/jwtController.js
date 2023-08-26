7ccc08647b99bbb4ac64fa5dab9da52f50269e0c
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2023-08-26 12:41:33 +0530

    fix: minor fix in jwt functions

--- api/controllers/jwtController.js
+++ api/controllers/jwtController.js
@@ -1,35 +1,35 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const accessTokenSecret = process.env.ACCESS_KEY_SECRET;
 const refreshTokenSecret = process.env.REFRESH_KEY_SECRET;
 
 const jwtController = {
-    signAccessToken: (regNo) => {
-        const payload = { regNo: regNo };
-        return jwt.sign(payload, accessTokenSecret, { expiresIn: '15s' });
-    },
-    
-    signRefreshToken: (regNo) => {
-        const payload = { regNo: regNo };
-        return jwt.sign(payload, refreshTokenSecret, { expiresIn: '1d' });
-    },
-    
-    verifyAccessToken: (token) => {
-        try {
-            return jwt.verify(token, accessTokenSecret);
-        } 
-        catch (error) {
-            throw new Error('Invalid access token');
-        }
-    },
-    
-    verifyRefreshToken: (token) => {
-        try {
-            return jwt.verify(token, refreshTokenSecret);
-        } 
-        catch (error) {
-            throw new Error('Invalid refresh token');
-        }
+  signAccessToken: (regNo) => {
+    const payload = { regNo: regNo };
+    return jwt.sign(payload, accessTokenSecret, { expiresIn: "15s" });
+  },
+
+  signRefreshToken: (regNo) => {
+    const payload = { regNo: regNo };
+    return jwt.sign(payload, refreshTokenSecret, { expiresIn: "1d" });
+  },
+
+  verifyAccessToken: (token) => {
+    try {
+      return jwt.verify(token, accessTokenSecret);
+    } catch (error) {
+      throw new Error("Invalid access token");
     }
+  },
+
+  verifyRefreshToken: (token) => {
+    try {
+      return jwt.verify(token, refreshTokenSecret);
+    } catch (error) {
+      throw new Error("Invalid refresh token");
+    }
+  },
 };
 
 module.exports = jwtController;
