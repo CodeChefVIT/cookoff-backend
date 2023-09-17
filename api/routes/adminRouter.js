09bfca69309f9f39ab8c8ecfd090997b161adec6
prateek-srivastava001 <prateeksrivastava201@gmail.com>
2023-09-17 12:51:56 +0530

    fix: fixed enabled/disabled round

--- api/routes/adminRouter.js
+++ api/routes/adminRouter.js
@@ -9,8 +9,8 @@ router.get('/dashboard', (req, res) => {
 });
 
 router.get('/getAllUsers', adminController.getAllUsers);
-router.get('/enableRound/:id', adminController.enableRound);
-router.get('/disableRound/:id', adminController.disableRound);
+router.get('/enableRound/:round', adminController.enableRound);
+router.get('/disableRound/:round', adminController.disableRound);
 router.post('/getUserByID', adminController.getUserbyID);
 router.post('/promoteUser', adminController.promoteUser);
 router.post('/banUser', adminController.banUser);
