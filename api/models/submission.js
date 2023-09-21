b0b100a28bfcd5721f9418fc60d3e8d313e46440
Vedant Matanhelia <vedant.matanhelia@gmail.com>
2023-09-21 17:05:58 +0530

    Formatting changes

--- api/models/submission.js
+++ api/models/submission.js
@@ -1,17 +1,20 @@
-const {Schema,model, default: mongoose} = require("mongoose");
+const { Schema, model, default: mongoose } = require("mongoose");
 
 const submission_schema = new Schema({
-    regNo : {type:String,required:true},
-    language_id : {type:Number,required:true},
-    code : {type:String,required:true},
-    max_score : {type:Number,required:true},
-    question_id : {type:mongoose.Schema.Types.ObjectId, ref : "Questions", required: true},
-    score : {type: Number},
-    lastResults : [{type: Boolean}],
-    allPassesAt: {type:Date}},
-    {timestamps : true}
-);
+    regNo: { type: String, required: true },
+    language_id: { type: Number, required: true },
+    code: { type: String, required: true },
+    max_score: { type: Number, required: true },
+    question_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Questions",
+        required: true,
+    },
+    score: { type: Number },
+    lastResults: [{ type: Boolean }],
+    allPassesAt: { type: Date },
+}, { timestamps: true });
 
-const submission_db = model("Submissions",submission_schema); 
+const submission_db = model("Submissions", submission_schema);
 
 module.exports = submission_db;
